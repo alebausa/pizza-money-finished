f3afc3a63158c91d8b14cf559e1614305b128c26
Ale Bausa <ale.bausa@gmail.com>
2022-11-19 14:36:13 +0100

    adds functions

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-function getTotal(){
-  const quantities = document.getElementsByClassName('quantity');
-  let total = 0;
-  [...quantities].forEach((elem) => total = total + parseInt(elem.value));
-  const totalSpan = document.getElementById('total');
-  totalSpan.innerHTML = total;
-}
-
-let id = 2;
-const table = document.getElementById('calculator');
+const tbody = document.getElementById('calculator');
+let id = 1;
 
 function createNewRow() {
-  const name = prompt("What's your friend's name?");
-  if (name) {
-    const newRow = document.createElement('tr');
-    newRow.id = id;
-    newRow.innerHTML = `<td>${name}</td>
-            <td><input class="quantity" type="number" value="0"/></td>
-            <td><button onclick="deleteRow(${id})">❌</button></td>`;
-    table.appendChild(newRow);
-    id = id + 1;
-  }
+  id = id + 1;
+  const newRow = document.createElement('tr');
+  newRow.id = id;
+  const name = prompt("What's your cheap friend's name?")
+  newRow.innerHTML = `
+    <td>${name}</td>
+    <td><input class="quantity" type="number" value="0" /></td>
+    <td><button onclick="deleteRow(${id})">❌</button></td>
+  `
+  tbody.appendChild(newRow);
 }
 
-function deleteRow(passId){
-  const row = document.getElementById(passId);
-  table.removeChild(row);
+function getTotal() {
+  let total = 0;
+  const quantities = document.getElementsByClassName('quantity');
+  [...quantities].forEach(elem => total = total + parseInt(elem.value))
+  const span = document.getElementById('total');
+  span.innerHTML = total;
+}
+
+function deleteRow(num) {
+  const tr = document.getElementById(num);
+  tbody.removeChild(tr)
   getTotal();
 }
+
+
+
